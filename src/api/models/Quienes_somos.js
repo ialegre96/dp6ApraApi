d4a4a247c58470e50fac7fb432c0e64b1ab06603
fabrizziodelaguila <fabrizzio.daguila@gmail.com>
2024-01-04 22:56:07 -0500

    modelquienessomos

---
+++ src/api/models/Quienes_somos.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const { Model } = require('objection');
+const baseModel = require('./base');
+const helper = require('./helper');
+
+class Quienessomos {
+	static get tableName() {
+		return 'quienessomos';
+	}
+
+	static get relationMappings() {}
+
+	static get jsonSchema() {
+		const schema = {
+			type: 'object',
+			requiered: ['telefono', 'email'],
+			properties: {
+				introduccionpap: { type: ['string', 'null'] },
+				estructurapap: { type: ['string', 'null'] },
+				representantespap: { type: ['string', 'null'] },
+				imgpap: { type: ['string', 'null'] },
+				reseñahispap: { type: ['string', 'null'] },
+				telefono: { type: ['string', 'null'] },
+				email: { type: ['string', 'null'] },
+				localespap: { type: ['string', 'null'] },
+			},
+		};
+		return schema;
+	}
+
+	static defaultColumns() {
+		return [
+			'introduccionpap',
+			'estructurapap',
+			'representantespap',
+			'imgpap',
+			'reseñahispap',
+			'telefono',
+			'email',
+			'localespap',
+		];
+	}
+
+	static listInfo() {
+		return this.query().select([
+			'introduccionpap',
+			'estructurapap',
+			'representantespap',
+			'imgpap',
+			'reseñahispap',
+			'telefono',
+			'email',
+			'localespap',
+		]);
+	}
+}
+
+module.exports = Quienessomos;
